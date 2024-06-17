3ffe0895b25c005d50be1f47a326a3699a3ff21c
Keaton Murray <murraykeaton422@gmail.com>
2024-06-17 04:28:10 -0400

    vite configuration completed

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    public function index() 
+    {
+        return view('index');
+    }
+
+    public function create()
+    {
+        
+    }
+
+    public function show(string $id)
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function edit(string $id)
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function remove(string $id)
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontController;
 
+/* DEFAULT ROUTE
 Route::get('/', function () {
     return view('welcome');
 });
+*/
+ 
+Route::get('/', [FrontController::class, 'index'])->name('welcome');
